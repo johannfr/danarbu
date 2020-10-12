00c834d21e10c21667c6504aa01a04a95fea8ce0
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2020-10-12 20:46:04 +0200

    🔧🐳 Splitting out my dev-execution-script

--- run.sh
+++ run.sh
@@ -9,11 +9,6 @@ do
 done
 
 docker build -t ${app}:latest -f docker/Dockerfile .
-# docker run -d --name ${app} -p 56733:80 \
-#     -v $(pwd):/app \
-#     -e FLASK_APP=main.py \
-#     -e FLASK_DEBUG=1 ${app} bash -c "while true ; do sleep 10 ; done"
-
 docker run -d -p 80:80 \
     --name=${app} \
     --network="host" \
